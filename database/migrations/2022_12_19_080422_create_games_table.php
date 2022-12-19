0b6c86c22186c793127ff7e69feaca422cbb2ed1
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2022-12-19 09:09:35 +0100

    Update 2022_12_19_080422_create_games_table.php

--- database/migrations/2022_12_19_080422_create_games_table.php
+++ database/migrations/2022_12_19_080422_create_games_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
         Schema::create('games', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->String("name");
+            $table->integer("price");
         });
     }
 
