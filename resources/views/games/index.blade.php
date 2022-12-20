27a2fcd507f19f87eae0a6d38f3a6686e2e78733
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2022-12-20 14:47:02 +0100

    added form to index blade

---
+++ resources/views/games/index.blade.php
@@ -0,0 +1,23 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                   @foreach ($games as $game)
+                       <p> Nom du jeu : {{$game->name}} | Prix : {{$game->price}}</p>
+                       <form action="{{ route('basket.index', $game) }}" method="POST">
+                            @csrf
+                            <button>Acheter</button>
+                        </form>
+                   @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
