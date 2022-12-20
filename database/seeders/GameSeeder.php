94c55602140b5a82d0a8eb74e39b3f27f6ea9914
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2022-12-20 14:26:01 +0100

    created seeder to list

--- database/seeders/GameSeeder.php
+++ database/seeders/GameSeeder.php
@@ -3,7 +3,12 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Ramsey\Uuid\Type\Integer;
+use Illuminate\Support\Carbon;
+
 
 class GameSeeder extends Seeder
 {
@@ -14,6 +19,13 @@ class GameSeeder extends Seeder
      */
     public function run()
     {
-        //
+        foreach (range(1, 20) as $index) {
+            DB::table('games')->insert([
+                'name' => Str::random(10),
+                'price' => random_int(1, 100),
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ]);
+        }
     }
 }
