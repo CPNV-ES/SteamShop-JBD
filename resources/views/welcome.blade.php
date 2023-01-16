a6035f445b0dce981c6faa754cc217eaf0683904
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-01-16 08:16:19 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,16 +9,17 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("hello") }}
+                    {{ __("Hit de la saison !") }}
                     @foreach ($games as $game)
                       
                        <form action="{{ route('basket.update') }}" method="post" class="bg-red-200 flex space-x-4 justify-items-stretch">
                             @csrf
                             @method('PATCH')
+                            <input hidden value="{{$game->id}}" name ="id">
                             <label> Nom du jeu : </label>
-                            <input type="text" readonly value=" {{$game->name}}" name="name">
+                            <input type="text" readonly value="{{$game->name}}" name="name">
                             <label> prix : </label>
-                            <input type="text" readonly value=" {{$game->price}}" name="price">
+                            <input type="text" readonly value="{{$game->price}}" name="price">
                             <button>Acheter</button>
 
                             <p class="bg-red-400 justify-self-end"> 5000% de réduction !!!!!!<p>
