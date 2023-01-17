6f3c42d2d78dffcc552900823fc5567b6cbc42c6
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-01-17 14:06:40 +0100

    games that have a publish date higher than today aren't rendered

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -14,7 +14,8 @@ class GameController extends Controller
      */
     public function index()
     {
-        $games = Game::all();
+        $games = Game::all()->where('publish_date', '<=', today());
+
         return view('games.index', ['games' => $games]);
     }
     public function listFamous()
